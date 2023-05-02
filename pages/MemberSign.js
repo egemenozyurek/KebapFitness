fd29cce3452097384a26684954685f5b5b23369e
egemenozyurek <ozyurekegemen95@gmail.com>
2023-05-02 19:48:59 +0100

    initial commit

---
+++ pages/MemberSign.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, StyleSheet, Alert } from "react-native";
+
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+function MemberSign({ navigation }) {
+  const [userName, setUserName] = useState(null);
+  const [userSurname, setUserSurname] = useState(null);
+  const [userAge, setUserAge] = useState(null);
+  const [userMail, setUserMail] = useState(null);
+  const [userHometown, setUserHometown] = useState(null);
+
+  function handleSubmit() {
+    if (!userName || !userSurname || !userAge || !userMail || !userHometown) {
+      Alert.alert("Kebap Fitness Salonu", "Bilgiler boş bırakılamaz!");
+      return;
+    }
+
+    const user = {
+      userName,
+      userSurname,
+      userMail,
+      userAge,
+      userHometown,
+    };
+    navigation.navigate("MemberResultScreen", { user });
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.input_container}>
+        <Input
+          label="Üye Adı"
+          placeholder="İsminiz, giriniz..."
+          onChangeText={setUserName} // bu şekilde direk değişen texti alır
+        />
+        <Input
+          label="Üye Soyadı"
+          placeholder="Soyisminiz, giriniz..."
+          onChangeText={setUserSurname}
+        />
+        <Input
+          label="Üye Yaşı"
+          placeholder="Yaşınızı giriniz..."
+          onChangeText={setUserAge}
+        />
+        <Input
+          label="Üye E-posta"
+          placeholder="E-posta adresinizi giriniz..."
+          onChangeText={setUserMail}
+        />
+        <Input
+          label="Üye Şehir"
+          placeholder="Şehir giriniz"
+          onChangeText={setUserHometown}
+        />
+      </View>
+      <Button text="Kaydı Tamamla" onPress={handleSubmit} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  input_container: {
+    width: "90%",
+  },
+});
+
+export default MemberSign;
---
+++ pages/Welcome.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import Button from "../components/Button/Button";
+
+const Welcome = ({ navigation }) => {
+  const goToMemberSign = () => {
+    navigation.navigate("Member Sign Screen");
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Kebap Fitness Salonu</Text>
+      <Button text={"Uye Kaydi Olustur"} onPress={goToMemberSign} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  header: {
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    margin: 10,
+  },
+});
+
+export default Welcome;
